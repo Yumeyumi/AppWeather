c69fea16b0df0dd67b948a39c158701d431df7ee
Beatriz Lopez <blopezdm@gmail.com>
2019-10-25 12:53:06 +0200

    Gestión de xml Item y RecyclerView

---
+++ app/src/main/java/com/utad/networking/DetailCity_Activity.kt
@@ -0,0 +1,12 @@
+package com.utad.networking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailCity_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_city_)
+    }
+}
